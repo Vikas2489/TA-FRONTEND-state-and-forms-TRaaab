eada849450f6a5a818d10a5c53199b38384a7aa1
vikas2489 <vk55002678@gmail.com>
2022-10-10 17:27:56 +0530

    create accordion app succesfully

---
+++ block-BRaabi/accordion-app/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles.css';
+import App from './Component/App';
+
+let rootElement = document.getElementById('root');
+
+ReactDOM.createRoot(rootElement).render(<App />);
